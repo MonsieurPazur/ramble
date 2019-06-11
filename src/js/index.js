023355a411b1523c98ab02f1b4f11430f9a51748
Michał Skwarek <michal.skwarek@hotmail.com>
2019-06-11 11:32:36 +0200

    Handle edges (saving)

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,76 @@
-// Main file that handles all the necessary logic
+// Main file that handles all the necessary logic.
 const Ramble = require('./ramble.js')
 let ramble = new Ramble()
 
-const Cytoscape = require('cytoscape')
-let cy = Cytoscape({
-	container: $('#graph-container'),
-	elements: [ // list of graph elements to start with
-		{ // node a
-			data: { id: 'a' }
-		},
-		{ // node b
-			data: { id: 'b' }
-		},
-		{ // edge ab
-			data: { id: 'ab', source: 'a', target: 'b' }
+const style = [
+	{
+		selector: 'node',
+		style: {
+			'background-color': '#666',
+			'label': 'data(name)'
 		}
-	],
-	style: [ // the stylesheet for the graph
-		{
-			selector: 'node',
-			style: {
-				'background-color': '#666',
-				'label': 'data(id)'
-			}
-		},
-
-		{
-			selector: 'edge',
-			style: {
-				'width': 3,
-				'line-color': '#ccc',
-				'target-arrow-color': '#ccc',
-				'target-arrow-shape': 'triangle'
-			}
+	},
+	{
+		selector: 'edge',
+		style: {
+			'width': 3,
+			'line-color': '#ccc',
+			'target-arrow-color': '#ccc',
+			'target-arrow-shape': 'triangle-backcurve',
+			'curve-style': 'unbundled-bezier'
 		}
-	],
-
-	layout: {
-		name: 'grid',
-		rows: 1
 	}
+]
+const cytoscape = require('cytoscape')
+
+// Register extensions.
+const edgehandles = require('cytoscape-edgehandles')
+cytoscape.use(edgehandles)
+
+// Initialize cytoscape.
+let cy = cytoscape({
+	container: $('#graph-container'),
+	elements: [],
+	style: style,
+	layout: {
+		name: 'circle'
+	},
+	minZoom: 0.5,
+	maxZoom: 2.0,
+})
+
+// Initialize edgehandles.
+const options = {
+	// Saving newly created edges
+	complete: function(sourceNode, targetNode, addedEles) {
+		let data = sourceNode.data()
+		data.target = targetNode.id()
+		ramble.dialogs.update(sourceNode.id(), data, function(result) {})
+	}
+}
+let eh = cy.edgehandles(options)
+
+ramble.dialogs.list(function(result) {
+	result.forEach(function(dialog) {
+		let data = dialog
+		data.id = dialog._id
+		cy.add({
+			group: 'nodes',
+			data: data
+		})
+	})
+	result.forEach(function(dialog) {
+
+		// If has specified target, create edge.
+		if (dialog.target) {
+			cy.add({
+				group: 'edges',
+				data: {
+					id: (dialog.id + '_' + dialog.target),
+					source: dialog.id,
+					target: dialog.target
+				}
+			})
+		}
+	})
 })
