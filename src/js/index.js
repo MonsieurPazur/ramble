2d7f12329ab38139fa67b25a554f0c845fb7f910
Michał Skwarek <michal.skwarek@hotmail.com>
2019-06-11 08:17:20 +0200

    Add graph lib

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,43 @@
 // Main file that handles all the necessary logic
 const Ramble = require('./ramble.js')
 let ramble = new Ramble()
+
+const Cytoscape = require('cytoscape')
+let cy = Cytoscape({
+	container: $('#graph-container'),
+	elements: [ // list of graph elements to start with
+		{ // node a
+			data: { id: 'a' }
+		},
+		{ // node b
+			data: { id: 'b' }
+		},
+		{ // edge ab
+			data: { id: 'ab', source: 'a', target: 'b' }
+		}
+	],
+	style: [ // the stylesheet for the graph
+		{
+			selector: 'node',
+			style: {
+				'background-color': '#666',
+				'label': 'data(id)'
+			}
+		},
+
+		{
+			selector: 'edge',
+			style: {
+				'width': 3,
+				'line-color': '#ccc',
+				'target-arrow-color': '#ccc',
+				'target-arrow-shape': 'triangle'
+			}
+		}
+	],
+
+	layout: {
+		name: 'grid',
+		rows: 1
+	}
+})
